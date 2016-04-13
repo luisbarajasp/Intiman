85f506d468a3a028090dfad1b25702a911fafe7c
luisbarajasp <luisbarajasp@gmail.com>
2016-04-12 19:48:20 -0500

    Fix: The filters of color and size fixed

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -42,10 +42,10 @@ class SalesController < ApplicationController
 
         @cloths = Cloth.order( "#{params[:sort] or 'created_at'} #{params[:order] or 'DESC'}")
 
-        @cloths = @cloths.where('? IN (sizes_n)', params[:size]) if params[:size]
+        @cloths = @cloths.select { |c| c.sizes_n.include? params[:size] } if params[:size]
         @cloths = @cloths.where('brand_n = ?', params[:brand]) if params[:brand]
         @cloths = @cloths.where('category_n = ?', params[:category]) if params[:category]
-        @cloths = @cloths.where('? IN (colors_n)', params[:color]) if params[:color]
+        @cloths = @cloths.select { |c| c.colors_n.include? params[:color] } if params[:color]
         @cloths = @cloths.where('discount_price < ?', params[:price]) if params[:price]
 
         @cos = Co.all
