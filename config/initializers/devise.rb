9fecbe1bf26c54fc237bbbf20fb5abe4cfc3933c
luisbarajasp <luisbarajasp@gmail.com>
2016-04-16 18:48:16 -0500

    Add: facebook authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,10 +12,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'no-reply@intiman.com'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'MyMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
@@ -236,6 +236,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"], scope: 'email', info_fields: 'email, first_name, last_name'
+  #config.omniauth :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
