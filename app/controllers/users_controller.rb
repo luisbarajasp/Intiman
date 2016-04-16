5100eeddc453a25fc20f2f271bef13af270ef9e5
luisbarajasp <luisbarajasp@gmail.com>
2016-04-16 18:49:14 -0500

    Add: facebook authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
       @user = current_user
   end
+
+  def finish_signup
+    if request.patch? && params[:user] # Revisa si el request es de tipo patch, es decir, llenaron el formulario y lo ingresaron
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        sign_in(@user, :bypass => true)
+        redirect_to root_path, notice: 'Hemos guardado tu email correctamente.'
+      else
+        @show_errors = true
+      end
+    end
+  end
+
+  private
+    def user_params
+      accessible = [ :first_name, :last_name, :email ] # extend with your own params
+      accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
+      params.require(:user).permit(accessible)
+    end
 end
