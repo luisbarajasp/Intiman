ce43a9df5409cb402343bd1d3c045fb7aa7e165b
luisbarajasp <luisbarajasp@gmail.com>
2016-07-28 17:14:55 -0500

    Add: hover div for the cloth in newreleases with the sizes available

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
       @sliders = HomeSlider.where('display = ?', true)
       @cloths = Cloth.order('created_at DESC').first(4)
+      @sizes = Size.order('created_at ASC')
   end
 
   def about
